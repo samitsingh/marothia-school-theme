ecda16f9592a471449e9c8c2119f1c781a6ccef1
Samit Singh <samitsingh.85@gmail.com>
2026-06-26 15:04:21 +0530

    Add hero block pattern

--- patterns/hero.php
+++ patterns/hero.php
@@ -1,1 +1,29 @@
+<?php
+/**
+ * Title: Hero
+ * Slug: marothia-school/hero
+ * Categories: featured
+ */
+?>
 
+<!-- wp:cover {"minHeight":650,"dimRatio":50,"overlayColor":"primary"} -->
+<div class="wp-block-cover" style="min-height:650px">
+    <span class="wp-block-cover__background has-primary-background-color has-background-dim-50"></span>
+
+    <div class="wp-block-cover__inner-container">
+
+        <!-- wp:heading {"textAlign":"center","level":1,"textColor":"white"} -->
+        <h1 class="has-text-align-center has-white-color has-text-color">
+        Building Bright Minds for a Better Tomorrow
+        </h1>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"align":"center","textColor":"white"} -->
+        <p class="has-text-align-center has-white-color has-text-color">
+        Inspiring Excellence • Nurturing Values • Empowering Futures
+        </p>
+        <!-- /wp:paragraph -->
+
+    </div>
+</div>
+<!-- /wp:cover -->
